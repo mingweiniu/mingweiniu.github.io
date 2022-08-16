a9968e93fd62a8317c3df51bca9549dbff016f63
Sidney_Niu <Sidney_Niu@users.noreply.github.com>
2022-08-16 16:00:05 +0800

    upload

--- upload.bat
+++ upload.bat
@@ -1,6 +1,6 @@
 git config --local user.name "Sidney_Niu"
 git config --local user.email "Sidney_Niu@users.noreply.github.com"
-git remote set-url origin git@SID:Sidney_Niu/mingweiniu.github.io.git
+git remote set-url origin git@SID:mingweiniu/mingweiniu.github.io.git
 
 git pull
 git add .
