7d9fb220e667cf29792a5ec5d8d7808825a93375
Sidney_Niu <Sidney_Niu@users.noreply.github.com>
2022-08-16 15:59:06 +0800

    upload

--- auto_upload.bat
+++
@@ -1,6 +0,0 @@
-git config --local user.email "mingweiniu@gmail.com"
-git config --local user.name "mingweiniu"
-git pull
-git add .
-git commit -m "all"
-git push
---
+++ upload.bat
@@ -0,0 +1,8 @@
+git config --local user.name "Sidney_Niu"
+git config --local user.email "Sidney_Niu@users.noreply.github.com"
+git remote set-url origin git@SID:Sidney_Niu/mingweiniu.github.io.git
+
+git pull
+git add .
+git commit -m "upload"
+git push
